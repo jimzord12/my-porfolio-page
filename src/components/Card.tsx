5b049b4b73d7a69f079ba5668ff3c6ebad54b06c
Dimitrios Stamatakis <45168324+jimzord12@users.noreply.github.com>
2025-01-24 21:32:33 +0200

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,25 @@
-// import React, { PropsWithChildren } from "react";
-// import GrainBG from "./GrainBG";
-// import { twMerge } from "tailwind-merge";
+import React, { PropsWithChildren } from "react";
+import GrainBG from "./GrainBG";
+import { twMerge } from "tailwind-merge";
 
-// const Card = ({
-//   children,
-//   className,
-// }: PropsWithChildren<{ className?: string }>) => {
-//   return (
-//     <div
-//       className={twMerge(
-//         "relative z-0 overflow-hidden rounded-3xl bg-gray-400/10 p-6 outline outline-2 outline-slate-400/5",
-//         className,
-//       )}
-//     >
-//       <GrainBG />
+const Card = ({
+  children,
+  className,
+}: PropsWithChildren<{ className?: string }>) => {
+  return (
+    <div
+      className={twMerge(
+        "relative z-0 overflow-hidden rounded-3xl bg-gray-400/10 p-6 outline outline-2 outline-slate-400/5",
+        className,
+      )}
+    >
+      <GrainBG />
 
-//       <div className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-16">
-//         {children}
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-16">
+        {children}
+      </div>
+    </div>
+  );
+};
 
-// export default Card;
+export default Card;
